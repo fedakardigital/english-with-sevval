dc1e389dfed1914555a5cddd44104593b4d5466c
petralux <petrakali34@gmail.com>
2026-07-04 02:07:39 +0300

    i18n locale tipi hatasi giderildi

--- i18n.ts
+++ i18n.ts
@@ -2,7 +2,8 @@ import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async ({ locale }) => {
   return {
-    locale, // İşte bu satır eksik olduğu için hata veriyordu!
+    // locale'nin undefined gelme ihtimaline karşı varsayılan bir değer atıyoruz
+    locale: locale || 'tr', 
     messages: (await import(`./messages/${locale}.json`)).default
   };
 });
