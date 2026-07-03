2cce882e96bcd7b279923ef8a892bc043c0ae3af
petralux <petrakali34@gmail.com>
2026-07-04 02:12:51 +0300

    i18n gereksiz dosyalar ve paketler kaldirildi

--- i18n.ts
+++
@@ -1,9 +0,0 @@
-import { getRequestConfig } from 'next-intl/server';
-
-export default getRequestConfig(async ({ locale }) => {
-  return {
-    // locale'nin undefined gelme ihtimaline karşı varsayılan bir değer atıyoruz
-    locale: locale || 'tr', 
-    messages: (await import(`./messages/${locale}.json`)).default
-  };
-});
