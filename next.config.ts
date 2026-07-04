1cd0477457198a80be85e175707f4287216909dd
petralux <petrakali34@gmail.com>
2026-07-04 14:40:36 +0300

    typescript hatalarini gormezden gel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // Build sırasında TypeScript hatalarını görmezden gelerek süreci hızlandırır
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
